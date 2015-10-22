202c03d5a62c292c1f11d60ffc8770f989f7734b
Kirtan <janmangal108@gmail.com>
2015-10-22 12:27:36 -0700

    Addition to the sign up page

--- app/src/main/java/com/sherpa/sherpa/MainActivity.java
+++ app/src/main/java/com/sherpa/sherpa/MainActivity.java
@@ -2,19 +2,22 @@ package com.sherpa.sherpa;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
+    protected TextView welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        welcome = (TextView) findViewById(R.id.welcome);
         ParseUser currentUser = ParseUser.getCurrentUser();
         if(currentUser == null)
         {
@@ -33,13 +37,15 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, currentUser.getUsername());
         }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.hide();
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery("Profile");
+        query.getInBackground("sbTnjse1B5", new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    welcome.setText("Welcome " + object.getString("firstname"));
+                } else {
+                    welcome.setText("something went wrong");
+                }
             }
         });
     }
--- app/src/main/java/com/sherpa/sherpa/SignUpActivity.java
+++ app/src/main/java/com/sherpa/sherpa/SignUpActivity.java
@@ -1,17 +1,20 @@
 package com.sherpa.sherpa;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -20,6 +23,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected EditText mPassword;
     protected EditText mEmail;
     protected Button mSignUpButton;
+    protected EditText phoneNumber;
+    protected EditText firstName;
+    protected EditText lastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +35,38 @@ public class SignUpActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
         mUsername = (EditText) findViewById(R.id.username);
         mPassword = (EditText) findViewById(R.id.password);
         mEmail = (EditText) findViewById(R.id.email);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        phoneNumber.setText(telephonyManager.getLine1Number());
         mSignUpButton = (Button) findViewById(R.id.signupButton);
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String fName = firstName.getText().toString();
+                String lName = lastName.getText().toString();
                 String username = mUsername.getText().toString();
                 String password = mPassword.getText().toString();
                 String email = mEmail.getText().toString();
+                String phone = phoneNumber.getText().toString();
+
 
                 username = username.trim();
                 password = password.trim();
                 email = email.trim();
-
-                if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                phone = phone.trim();
+                fName = fName.trim();
+                lName = lName.trim();
+                if (username.isEmpty() ||
+                        password.isEmpty() ||
+                        email.isEmpty() ||
+                        phone.isEmpty() ||
+                        fName.isEmpty() ||
+                        lName.isEmpty()) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
                     builder.setMessage(R.string.error_message)
                             .setTitle(R.string.sign_up_title_error)
@@ -53,23 +75,39 @@ public class SignUpActivity extends AppCompatActivity {
                     dialog.show();
                 } else {
                     setProgressBarIndeterminateVisibility(true);
-                    ParseUser user = new ParseUser();
+                    final ParseUser user = new ParseUser();
                     user.setUsername(username);
                     user.setPassword(password);
                     user.setEmail(email);
-                    user.signUpInBackground(new SignUpCallback() {
+                    final ParseObject profile = new ParseObject("Profile");
+                    profile.put("phone", phone);
+                    profile.put("firstname", fName);
+                    profile.put("lastname", lName);
+                    profile.saveInBackground(new SaveCallback() {
                         @Override
                         public void done(ParseException e) {
-                            setProgressBarIndeterminateVisibility(false);
                             if (e == null) {
-                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(intent);
+                               // Log.d(TAG, "User update saved!");
+                                //Log.d(TAG, "The object id (from User) is: " + user.getParseObjectId());
                             } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
-                                builder.setMessage(e.getMessage())
-                                        .setTitle(R.string.sign_up_title_error)
+                               // Log.d(TAG, "User update error: " + e);
+                            }
+                        }
+                });
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        setProgressBarIndeterminateVisibility(false);
+                        if (e == null) {
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        } else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                            builder.setMessage(e.getMessage())
+                                    .setTitle(R.string.sign_up_title_error)
                                         .setPositiveButton(R.string.ok, null);
                                 AlertDialog dialog = builder.create();
                                 dialog.show();
@@ -79,15 +117,5 @@ public class SignUpActivity extends AppCompatActivity {
                 }
             }
         });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.hide();
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
     }
 }
