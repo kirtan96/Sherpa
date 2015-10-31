331c401fca1f3a5eeadbd60aee87b9d9579f4b67
monukadakia <monukadakia@gmail.com>
2015-10-30 20:49:02 -0700

    Payment with Paypal is added

---
+++ app/src/main/java/com/sherpa/sherpa/Activites/PayPal_Selection.java
@@ -0,0 +1,59 @@
+package com.sherpa.sherpa.Activites;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.sherpa.sherpa.R;
+
+public class PayPal_Selection extends ActionBarActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_pal__selection);
+
+        Button button_paypal_sdk= (Button) findViewById(R.id.button_paypal_sdk);
+        button_paypal_sdk.setOnClickListener(this);
+
+       // Button button_paypal_mpl= (Button) findViewById(R.id.button_paypal_mpl);
+        //button_paypal_mpl.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.button_paypal_sdk:
+                // call paypal sdk intent
+                startActivity(new Intent(PayPal_Selection.this,PayPal_sdk.class));
+
+                break;
+
+          /*  case R.id.button_paypal_mpl:
+                //call mpl activity
+
+                startActivity(new Intent(PayPal_Selection.this,checkout.class));
+
+                break;*/
+
+            default:
+
+                break;
+
+
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/sherpa/sherpa/Activites/ViewSherpaProfile.java
+++ app/src/main/java/com/sherpa/sherpa/Activites/ViewSherpaProfile.java
@@ -6,6 +6,8 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,8 +39,8 @@ public class ViewSherpaProfile extends AppCompatActivity {
         query.findInBackground(new FindCallback<ParseUser>() {
             @Override
             public void done(List<ParseUser> list, ParseException e) {
-                for(ParseUser u: list) {
-                    if(u.getUsername().equals(username)) {
+                for (ParseUser u : list) {
+                    if (u.getUsername().equals(username)) {
                         user = u;
 
                         final ImageView profilePic = (ImageView) findViewById(R.id.profilePicture);
@@ -65,12 +67,9 @@ public class ViewSherpaProfile extends AppCompatActivity {
                         placeCity.setText(placeCity.getText() + user.getString("gcity") + ":");
                         places.setText(user.getString("places"));
                         String hour;
-                        if(user.getString("costType").equals("H"))
-                        {
+                        if (user.getString("costType").equals("H")) {
                             hour = "hour";
-                        }
-                        else
-                        {
+                        } else {
                             hour = "day";
                         }
                         cost.setText(cost.getText() + "$" + user.getDouble("cost") + "/" + hour);
@@ -78,8 +77,36 @@ public class ViewSherpaProfile extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.checkout, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_checkout) {
+
+            navigateToCheckout();
+        }
 
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void navigateToCheckout()
+    {
+        Intent intent = new Intent(this, PayPal_Selection.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 }
