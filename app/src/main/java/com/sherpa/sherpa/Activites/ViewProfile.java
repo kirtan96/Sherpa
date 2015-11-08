a1bd78a3fcc88fc3eb27a2be28512fb5e858ce5b
Kirtan <janmangal108@gmail.com>
2015-11-08 15:08:57 -0800

    rating view in profile is done

--- app/src/main/java/com/sherpa/sherpa/Activites/ViewProfile.java
+++ app/src/main/java/com/sherpa/sherpa/Activites/ViewProfile.java
@@ -11,20 +11,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.sherpa.sherpa.R;
 import com.sherpa.sherpa.UserList;
+
+import java.util.List;
 
 /**
  * Created by Kirtan on 10/21/15.
  */
 
 public class ViewProfile extends AppCompatActivity {
+
+    int rater;
+    float rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +87,7 @@ public class ViewProfile extends AppCompatActivity {
                 navigateToChatList();
             }
         });
+        updateRating(ParseUser.getCurrentUser().getUsername());
     }
 
     private void navigateToChatList() {
@@ -128,5 +138,28 @@ public class ViewProfile extends AppCompatActivity {
             ParseUser.getCurrentUser().put("online", true);
             ParseUser.getCurrentUser().saveInBackground();
         }
+        updateRating(ParseUser.getCurrentUser().getUsername());
+    }
+
+    public void updateRating(String name){
+        ParseQuery<ParseObject> pq = ParseQuery.getQuery("Rating");
+        pq.whereEqualTo("RateTo", name);
+        pq.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                rater = 0;
+                rating = 0;
+                for (ParseObject po : list) {
+                    rating = rating + (float) po.getDouble("rating");
+                    rater++;
+                }
+
+                rating = rating / rater;
+                RatingBar ratingBar1 = (RatingBar) findViewById(R.id.ratingBar);
+                ratingBar1.setRating(rating);
+                TextView raters = (TextView) findViewById(R.id.raters);
+                raters.setText("" + rater);
+            }
+        });
     }
 }
