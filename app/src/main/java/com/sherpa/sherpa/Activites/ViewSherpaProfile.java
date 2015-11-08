e05618b20954613f86079a0e07fb01502efea8c6
Swonboyyash <swonboy.yash@gmail.com>
2015-11-07 21:14:16 -0800

    Now, a tourist can rate for a Sherpa

--- app/src/main/java/com/sherpa/sherpa/Activites/ViewSherpaProfile.java
+++ app/src/main/java/com/sherpa/sherpa/Activites/ViewSherpaProfile.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
@@ -28,10 +29,13 @@ import java.util.List;
  * Created by Kirtan on 10/21/15.
  */
 
-public class ViewSherpaProfile extends AppCompatActivity {
+public class ViewSherpaProfile extends AppCompatActivity implements RatingBar.OnRatingBarChangeListener {
 
     ParseUser user;
     String sherpaName;
+    private static final float DEFAULT_RATING = 0;
+    public RatingBar ratingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +58,7 @@ public class ViewSherpaProfile extends AppCompatActivity {
                 for (ParseUser u : list) {
                     if (u.getUsername().equals(username)) {
                         user = u;
+                        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
 
                         final ImageView profilePic = (ImageView) findViewById(R.id.profilePicture);
                         TextView name = (TextView) findViewById(R.id.name);
@@ -65,6 +70,7 @@ public class ViewSherpaProfile extends AppCompatActivity {
                         email.setText(user.getEmail());
                         TextView phone = (TextView) findViewById(R.id.phone);
                         phone.setText(user.getString("phone"));
+
 
                         ParseFile file = user.getParseFile("pp");
                         file.getDataInBackground(new GetDataCallback() {
@@ -85,6 +91,13 @@ public class ViewSherpaProfile extends AppCompatActivity {
                             hour = "day";
                         }
                         cost.setText(cost.getText() + "$" + user.getDouble("cost") + "/" + hour);
+                        ratingBar.setIsIndicator(true);
+                        if(user.containsKey("rating")) {
+                            ratingBar.setRating((float) user.getDouble("rating"));
+                        }
+                        else {
+                            ratingBar.setRating(0);
+                        }
                     }
                 }
             }
@@ -95,6 +108,14 @@ public class ViewSherpaProfile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 navigateToChat();
+            }
+        });
+
+        Button rateButton = (Button) findViewById(R.id.rateButton);
+        rateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToRating();
             }
         });
     }
@@ -121,6 +142,13 @@ public class ViewSherpaProfile extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void navigateToRating()
+    {
+        Intent intent = new Intent(this, RatingSherpa.class);
+        intent.putExtra("username", sherpaName);
+        startActivity(intent);
+    }
+
     private void navigateToChat()
     {
         Intent intent = new Intent(this, Chat.class);
@@ -144,5 +172,10 @@ public class ViewSherpaProfile extends AppCompatActivity {
             ParseUser.getCurrentUser().put("online", true);
             ParseUser.getCurrentUser().saveInBackground();
         }
+    }
+
+    @Override
+    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+        ratingBar.setOnRatingBarChangeListener((RatingBar.OnRatingBarChangeListener)new RatingSherpa().ratingSherpa);
     }
 }
